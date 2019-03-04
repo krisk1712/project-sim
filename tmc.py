b0afc8473bc97f66cd25b0832aa0c56b1ee9dfce
krisk1712 <17947361+krisk1712@users.noreply.github.com>
2019-03-04 17:29:06 +0530

    FINAL I THINK

--- tmc.py
+++ tmc.py
@@ -12,12 +12,124 @@ def client_program():
         if not data:
             break
         # print("from connected user: " + str(data))
+        cmd = pickle.loads(data)
+        # if cmd == 'notm':
+        #     print("NO TELECOMMAND PROSSED DATA OVER SASTELLITE...........")
+        #     continue
+        if  cmd[2][2]=="3A326910":
+            tas = "THREE AXIS STABLIZE"
+            print("The Command has been received from the ground station...TAS")
+            mess = "--->>Received command THREE AXIS STABLIZE <<---"
+            client_socket.send(mess.encode())
+            pass
 
-        command = pickle.loads(data)
-        print(command)
-        print("the packet received")
-        send = "The command  has been received"
-        client_socket.send(send.encode())
+        elif cmd[2][2] =="8789AC43":
+            spd = "SOLAR PANEL DEPLOYMENT"
+
+            print("The Command has been received from the ground station... SPD")
+            mess = "--->>Received command SOLAR PANEL DEPLOYMENT <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="69984ACD":
+            ea = "EARTH ACQUSITION "
+            print("The Command has been received from the ground station...EA")
+            mess = "--->>Received command EARTH ACQUSITION <<---"
+            client_socket.send(mess.encode())
+            pass
+        elif cmd[2][2] =="ACD1437DF":
+            sa = "SUN ACQUSITION"
+            print("The Command has been received from the ground station...SA")
+            mess = "--->>Received command SUN ACQUSITION <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="0A010ECD":
+            pit = "PCS_INIT_TERM"
+
+            print("The Command has been received from the ground station...PIT")
+            mess = "--->>Received command PCS_INIT_TERM <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="ADCFE1235":
+            sud = "SS_UPDATE_DISABLE"
+
+            print("The Command has been received from the ground station...SUD")
+            mess = "--->>Received command SS_UPDATE_DISABLE <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="987ADC344":
+            ton = "TRANSMITTER ON"
+
+            print("The Command has been received from the ground station...TON")
+            mess = "--->>Received command TRANSMITTER ON <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="0A14CD13":
+            opon = "ORIGAMI PAYLOAD ON"
+
+            print("The Command has been received from the ground station...OPON")
+            mess = "--->>Received command ORIGAMI PAYLOAD ON <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="76AC1340":
+            opoff = "ORIGAMI PAYLOAD OFF"
+
+            print("The Command has been received from the ground station...OPOFFF")
+            mess = "--->>Received command ORIGAMI PAYLOAD OFF <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="876ADEF5":
+            sue = "SS UPDATE ENABLE"
+
+            print("The Command has been received from the ground station...SUE")
+            mess = "--->>Received command SS UPDATE ENABLE <<---"
+            client_socket.send(mess.encode())
+            pass
+
+        elif cmd[2][2] =="68AECD120":
+            thk = "TRANSMIT HK"
+
+            print("The Command has been received from the ground station...THK")
+            mess = "--->>Received command TRANSMIT HK <<---"
+            client_socket.send(mess.encode())
+            pass
+        elif cmd[2][2] =="AFFEC34A":
+            thk = "SAT STAT"
+
+            print("The Command has been received from the ground station...SATSTAT")
+            mess = "--->>Received command SAT STAT <<---"
+            client_socket.send(mess.encode())
+            pass
+        elif cmd[2][2] =="BA53A980":
+            thk = "BAT STAT"
+
+            print("The Command has been received from the ground station...BATSTAT")
+            mess = "--->>Received command BAT STAT <<---"
+            client_socket.send(mess.encode())
+            pass
+        elif cmd[2][2] =="231876A7":
+            thk = "HLTH"
+
+            print("The Command has been received from the ground station...HLTH")
+            mess = "--->>Received command HLTH <<---"
+            client_socket.send(mess.encode())
+            pass
+        elif cmd[2][2] =="C4579850":
+            thk = "CHRG"
+
+            print("The Command has been received from the ground station...CHRG")
+            mess = "--->>Received command CHRG <<---"
+            client_socket.send(mess.encode())
+            pass
+        else:
+            print("NO COMMAND TO BE RECOG BY THE SAT.....")
+            break
     client_socket.close() 
 
 
--- tms.py
+++ tms.py
@@ -50,18 +50,74 @@ def telecmd_recv(s):
         new = tmdb.readlines()
         pack = new[red + 1]
         print(pack)
-        packt= pickle.dumps(pack)
-        conn.send(packt)
-        tcdb = open("tcdb.txt",'a')        
+        print("THE ABOVE IS THE READ FILE DATA")
+        if pack.strip() == "THREE AXIS STABLIZE":
+            sis = ["4EA36B01","83EC",["AA55BB66","1A0E","3A326910","0101"],"1212"]
+            sis_proc = pickle.dumps(sis)
+            conn.send(sis_proc)
+            pass
+        elif pack.strip() == "SOLAR PANEL DEPLOYMENT":
+            dsp = ["4EA36B01","83EC",["AA55BB66","1A0E","8789AC43","0101"],"1212"]
+            dsp_proc = pickle.dumps(dsp)
+            conn.send(dsp_proc)
+            pass
+        elif pack.strip() == "EARTH ACQUSITION ":
+            ton = ["4EA36B01","83EC",["AA55BB66","1A0E","69984ACD","0101"],"1212"]
+            ton_proc = pickle.dumps(ton)
+            conn.send(ton_proc)
+            pass
+        elif pack.strip() == "SUN ACQUSITION":
+            ea = ["4EA36B01","83EC",["AA55BB66","1A0E","ACD1437DF","0101"],"1212"] 
+            ea_proc  = pickle.dumps(ea)
+            conn.send(ea_proc)
+            pass
+        elif pack.strip() == "PCS_INIT_TERM":
+            sa = ["4EA36B01","83EC",["AA55BB66","1A0E","0A010ECD","0101"],"1212"] 
+            sa_proc = pickle.dumps(sa)
+            conn.send(sa_proc)
+            pass
+        elif pack.strip() == "SS_UPDATE_DISABLE":
+            t_off = ["4EA36B01","83EC",["AA55BB66","1A0E","ADCFE1235","0101"],"1212"]  
+            t_off_proc   = pickle.dumps(t_off)
+            conn.send(t_off_proc)
+            pass
+        elif pack.strip() == "TRANSMITTER ON":
+            pd = ["4EA36B01","83EC",["AA55BB66","1A0E","987ADC34","0101"],"1212"] 
+            pd_proc  = pickle.dumps(pd)
+            conn.send(pd_proc)
+            pass
+        elif pack.strip() == "ORIGAMI PAYLOAD ON":
+            oson = ["4EA36B01","9A7D",["AA55BB66","1A0E","0A14CD13","0101"],"1212"]
+            oson_proc = pickle.dumps(oson)
+            conn.send(oson_proc)
+            pass
+        elif pack.strip() == "ORIGAMI PAYLOAD OFF":
+            osoff = ["4EA36B01","83EC",["AA55BB66","1A0E","76AC1340","0101"],"1212"] 
+            osoff_proc = pickle.dumps(osoff)
+            conn.send(osoff_proc)
+            pass
+        elif pack.strip() == "SS UPDATE ENABLE":
+            hk = ["4EA36B01","83EC",["AA55BB66","1A0E","876ADEF5","0101"],"1212"] 
+            hk_proc  = pickle.dumps(hk)
+            conn.send(hk_proc)
+            pass
+        elif pack.strip() == "TRANSMIT HK":
+            hk = ["4EA36B01","83EC",["AA55BB66","1A0E","68AECD120","0101"],"1212"] 
+            hk_proc  = pickle.dumps(hk)
+            conn.send(hk_proc)
+            pass
+        else:
+            print("NO SUCH TELE_COMMAND")
+            pass       
         red = red + 2
     except IndexError:
         print("NO TELECOMMAND SORRY.....")
-        contm = "NO TELECOMMAND IN THE SATELLITE QUE..."
+        contm = "notm"
         cont = pickle.dumps(contm)
         conn.send(cont)
-    s.enter(10, 5, telecmd_recv, (s,))                      # calling event scheduler at the 10th second only for the function execution
+    s.enter(10, 3, telecmd_recv, (s,))                      # calling event scheduler at the 10th second only for the function execution
 
 s.enter(1, 1, telemet_send, (s,))                           # 
-s.enter(10, 1, telecmd_recv, (s,))                          #    Function calling on all the side to run the diffrent function 
+s.enter(10, 3, telecmd_recv, (s,))                          #    Function calling on all the side to run the diffrent function 
 s.run() 
 
